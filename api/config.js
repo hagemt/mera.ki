0892e133e58d44d1c5f73d365b10bd536ef3594f
Tor Hagemann <tohagema@cisco.com>
2017-11-06 00:15:10 -0800

    api: use config to dynamically load secrets

--- api/config.js
+++ api/config.js
@@ -3,13 +3,6 @@ const path = require('path')
 
 const _ = require('lodash')
 
-const secrets = {}
-try {
-	Object.assign(secrets, require('./secrets.js'))
-} catch (error) {
-	console.error(error) // eslint-disable-line no-console
-}
-
 const DEFAULT_PORT_NUMBER = 8080 // alternative HTTP
 
 const PROJECT_ROOT = path.resolve(__dirname, '..')
@@ -19,22 +12,50 @@ const LOG_PATH = path.resolve(PROJECT_ROOT, 'server.log')
 
 // simple alternative to require('config'):
 
-const all = Object.freeze({
-	data: Object.freeze({
+const config = {
+	data: {
 		path: process.env.DB_PATH || DATA_PATH,
-	}),
-	log: Object.freeze({
+	},
+	log: {
 		level: process.env.LOG_LEVEL || 'debug',
 		name: process.env.LOG_NAME || 'mera.ki',
 		path: process.env.LOG_PATH || LOG_PATH,
-	}),
-	secrets: Object.freeze(secrets),
-	server: Object.freeze({
+	},
+	server: {
 		path: process.env.SERVED_PATH || SERVER_PATH,
 		port: process.env.PORT || DEFAULT_PORT_NUMBER,
-	}),
-})
+	},
+}
+
+const deepFreeze = (object, visited = new WeakSet()) => {
+	if (!object || typeof object !== 'object' || visited.has(object)) return
+	visited.add(object) // prevents recursion over nested Object references
+	for (const key of Object.keys(object)) deepFreeze(object[key], visited)
+	Object.freeze(object) // base case: prevent modifications (on post-order)
+}
+
+const getValue = (frozenObject, maybeKey) => {
+	const key = maybeKey || '' // prevent undefined
+	if (_.has(config, key)) return _.get(config, key)
+	throw new Error(`FATAL: missing config (${key})`)
+}
+
+const secrets = {}
+try {
+	// eslint-disable-next-line global-require
+	Object.assign(secrets, require('./secrets.js'))
+} catch (error) {
+	// eslint-disable-next-line no-console
+	console.error(error, 'WARNING: missing api/secrets.js')
+} finally {
+	deepFreeze(config)
+	deepFreeze(secrets)
+}
 
 module.exports = {
-	get: key => _.get(all, key),
+	get: key => getValue(config, key),
+	getSecrets: (key) => {
+		if (!key) return secrets
+		return getValue(secrets, key)
+	},
 }
